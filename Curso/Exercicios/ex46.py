9027439d799773f666aa28b6914687433411d22a
Italo_Leonardo <italoleonardo69@gmail.com>
2024-04-13 21:33:29 -0300

    add

--- Curso/Exercicios/ex46.py
+++ Curso/Exercicios/ex46.py
@@ -1,3 +1,5 @@
-for c in range(10, 0, -1):
+from time import sleep
+for c in range(10, -1, -1):
     print('Falta {} segundos para o Fim do ano!'.format(c))
+    sleep(0.5)
 print('FEZ ANO NOVO!!!!!!!!!!!!!!')
