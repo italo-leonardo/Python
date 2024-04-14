065a69dade04046f2f0de957c45cdd910b7c969c
Italo_Leonardo <italoleonardo69@gmail.com>
2024-04-13 21:53:52 -0300

    add

--- Curso/Exercicios/ex48.py
+++ Curso/Exercicios/ex48.py
@@ -1,5 +1,7 @@
 s = 0
+cont = 0
 for c in range(1, 500 , 2): # Numero imparres
     if c % 3 == 0: # Calculo para saber se é multiplo de 3
         s += c # Soma de todos os multiplos de 3 
-print('A soma de todos os numeros impares de 1 a 500 que são multiplo de 3: {}'.format(s))
+        cont += 1
+print('A soma de todos os {} numeros impares de 1 a 500 que são multiplo de 3 é: {}'.format(cont, s))
