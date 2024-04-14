b8d8b827a40fb8a2b3d5090881a4cfc564c54082
Italo_Leonardo <italoleonardo69@gmail.com>
2024-04-13 21:28:11 -0300

    add

--- Curso/Exercicios/ex55.py
+++ Curso/Exercicios/ex55.py
@@ -1,8 +1,11 @@
 maior = 0
-for c in range(0, 5):
+menor = 99999
+for c in range(0, 3):
     peso = float(input('Digite o seu peso: '))
-    menor = peso
     if peso > maior:
         maior = peso
+    if peso < menor:
+        peso = menor
 print(maior)
+print(menor)
 
--- Curso/Exercicios/ex56.py
+++ Curso/Exercicios/ex56.py
@@ -3,7 +3,7 @@ soma = 0
 velho = 0
 nomeVelho = ''
 menos20 = 0
-for c in range(0, 2):
+for c in range(0, 4):
     nome = input('Digite seu nome: ')
     idade = int(input('Digite seu idade: '))
     sexo = int(input('Digite 1 homen e 2 Mulher: '))
@@ -17,6 +17,6 @@ for c in range(0, 2):
         if idade < 20:
             menos20 +=1 
         
-print('A media da Idade do Grupo é de {} Anos!'.format(soma))
-print('O nome do Humem mais velho é {}'.format(nomeVelho))
-print('{} tem manos de 20 anos'.format(menos20))
+print('A media da Idade do Grupo é de {} Anos.'.format(soma))
+print('O nome do Humem mais velho é {}.'.format(nomeVelho))
+print('{} Mulheres tem manos de 20 anos.'.format(menos20))
