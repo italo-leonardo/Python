4dd30334436939a83726c905495f33a7492c65e9
Italo_Leonardo <italoleonardo69@gmail.com>
2024-04-13 22:26:06 -0300

    add

--- Curso/Exercicios/ex52.py
+++ Curso/Exercicios/ex52.py
@@ -1,8 +1,17 @@
 n = int(input('Digite um numero: '))
-s = 0
-for c in range(0, n+1):
-    if c % 2 != 0 and c > 1:
-        if c % 3 == 0:
-            print(c)
+t = 0
+for c in range(1, n + 1):
+    if n % c == 0:
+        print('\033[33m', end='')
+        t += 1
+    else:
+        print('\033[31m', end='')
+    print('{}'.format(c), end=' ')
+print('\n')
+print('O numero {} foi divisivel {} vezes'.format(n, t))
+if t == 2:
+    print('È por isso que ele é PRIMO!')
+else:
+    print('È por isso que ele Não é PRIMO!')
 
        
