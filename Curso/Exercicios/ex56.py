31287a724ffcf2b1e86f2a1a4c982ec57248837d
Italo_Leonardo <italoleonardo69@gmail.com>
2024-04-27 15:06:25 -0300

    add

--- Curso/Exercicios/ex56.py
+++ Curso/Exercicios/ex56.py
@@ -3,10 +3,12 @@ soma = 0
 velho = 0
 nomeVelho = ''
 menos20 = 0
-for c in range(0, 4):
+for c in range(1, 5):
+    print(f'------ {c}º Pessoa ------')
     nome = input('Digite seu nome: ')
     idade = int(input('Digite seu idade: '))
     sexo = int(input('Digite 1 homen e 2 Mulher: '))
+    print('\n')
     media += idade
     soma = media / 4
     if sexo == 1:
@@ -18,5 +20,5 @@ for c in range(0, 4):
             menos20 +=1 
         
 print('A media da Idade do Grupo é de {} Anos.'.format(soma))
-print('O nome do Humem mais velho é {}.'.format(nomeVelho))
+print('O nome do Humem mais velho é {} com {} Anos.'.format(nomeVelho,velho))
 print('{} Mulheres tem manos de 20 anos.'.format(menos20))
