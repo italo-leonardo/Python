bf9018adcf105753da9b62ba4767f112565eb937
Italo_Leonardo <italoleonardo69@gmail.com>
2024-04-27 14:27:58 -0300

    add

--- Curso/Exercicios/ex54.py
+++ Curso/Exercicios/ex54.py
@@ -1,11 +1,13 @@
+from datetime import date
 maior = 0
 menor = 0
-for c in range(0, 7):
-    data = int(input('Data de nacimento: '))
-    s = 2024 - data
+data_atual = date.today().year
+for c in range(1, 8):
+    data = int(input(f'Data de nacimento da {c}º pessoa: '))
+    s = data_atual - data
     if s >= 18:
         maior +=1
-    else:
+    if s < 18:
         menor +=1 
-print('{} maior de idade.'.format(maior))
-print('{} menor de idade.'.format(menor))
+print('Ao total tivemos {} pessoas maior de idade.'.format(maior))
+print('Ao total tivemos {} menor de idade.'.format(menor))
