42ff5a550140ead35ca000a7743309cef71a5de3
Italo_Leonardo <italoleonardo69@gmail.com>
2024-04-30 19:30:07 -0300

    Create funcoes.py

---
+++ Faculdade/Aula04/funcoes.py
@@ -0,0 +1,11 @@
+def linha():
+    for i in range(20):
+        print("*", end="")
+    print("")
+
+print("AULA DE LAB. PROGRAMAÇÃO")
+linha()
+print("PROF. CAETANO")
+linha()
+print("AULA NA UNIFAP")
+linha()
