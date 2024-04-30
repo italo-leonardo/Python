7c2b4c20e29ea03b76fa360db2e2c32254aa9fb4
Italo_Leonardo <italoleonardo69@gmail.com>
2024-04-30 19:43:29 -0300

    add

---
+++ Faculdade/Aula04/funcoes2.py
@@ -0,0 +1,11 @@
+def linha(quantidade):
+    for i in range(quantidade):
+        print("*", end="")
+    print("")
+
+print("AULA DE LAB. PROGRAMAÇÃO")
+linha(int(input("Digite um valor:")))
+print("PROF. CAETANO")
+linha(15)
+print("AULA NA UNIFAP")
+linha(20)
