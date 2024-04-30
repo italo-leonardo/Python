8225c2460d34888b007fa4755e3f5f91710a3a36
Italo_Leonardo <italoleonardo69@gmail.com>
2024-04-30 19:57:46 -0300

    add

--- Faculdade/Aula04/funcoes3.py
+++ Faculdade/Aula04/funcoes3.py
@@ -1,4 +1,4 @@
-def linha(quantidade, texto):
+def linha(quantidade, texto="*"):
     for i in range(quantidade):
         print(texto, end="")
     print("")
@@ -6,6 +6,6 @@ def linha(quantidade, texto):
 print("AULA DE LAB. PROGRAMAÇÃO")
 linha(int(input("Digite um valor:")), input("Digite um texto: "))
 print("PROF. CAETANO")
-linha(15, "*")
+linha(15)
 print("AULA NA UNIFAP")
 linha(20, "$")
