63e1a795e00d66269fe97b87fcfabc26a15d56a4
Italo_Leonardo <italoleonardo69@gmail.com>
2024-04-30 21:20:23 -0300

    add

---
+++ Faculdade/Aula04/funcoes6.py
@@ -0,0 +1,7 @@
+resposta = 0 #Variavel global
+def soma(n1, n2):
+    resul = n1 + n2
+    return resul
+
+resposta = soma(int(input('Digite o valor: ')), int(input('Digite o valor: ')))
+print(resposta)
