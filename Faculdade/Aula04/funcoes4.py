48a3c136535fd0514444ef3d4e68c14ec4fabaec
Italo_Leonardo <italoleonardo69@gmail.com>
2024-04-30 21:10:55 -0300

    add

---
+++ Faculdade/Aula04/funcoes4.py
@@ -0,0 +1,4 @@
+def soma(n1, n2):
+    (n1 + n2)
+
+soma(int(input('Digite o valor: ')), int(input('Digite o valor: ')))
---
+++ Faculdade/Aula04/funcoes5.py
@@ -0,0 +1,6 @@
+#variavel local
+def soma(n1, n2):
+    resul = n1 + n2
+    print(resul)
+
+soma(int(input('Digite o valor: ')), int(input('Digite o valor: ')))
