796483ae25df9eecbd2c2ca617a0a29ef26bec02
Italo_Leonardo <italoleonardo69@gmail.com>
2024-05-26 12:37:54 -0300

    add

--- CursoPython/Exercicios/ex62.py
+++ CursoPython/Exercicios/ex62.py
@@ -1,15 +1,17 @@
-primeiro = int(input('Primeiro Termo: '))
+"""primeiro = int(input('Primeiro Termo: '))
 razao = int(input('Razão: '))
 cont = 0
 pula = 0
 guarda = 0
-while cont < 3:
+termos = 0
+while cont < 10:
     soma = primeiro + pula
     pula += razao
     print(soma, end=" ->")
     guarda = soma
     cont += 1
-print("FIM")
+    termos +=1 
+print("PAUSA")
 pert = int(input("\nQuantos termos a mais deseja ver: "))
 while pert > 0:
     if pert != 0:
@@ -22,5 +24,24 @@ while pert > 0:
             print(soma, end=" ->")
             guarda = soma
             cont += 1
+            termos +=1
+    print("PAUSA")
     pert = int(input("\nQuantos termos a mais deseja ver: "))
-print('FIM DO PROGRAMA')
+print(f'Progressão finalizada com {termos} termos mostrados.')"""
+
+primeiro = int(input('Primeiro Termo: '))
+razao = int(input('Razão: '))
+cont = 0
+pula = 0
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        soma = primeiro + pula
+        pula += razao
+        print(soma, end=" ->")
+        cont += 1
+    print("PAUSA")
+    mais = int(input("Quantos termos você quer mostra a mais? "))
+print(f'Progressão finalizada com {total} termos mostrados.')
