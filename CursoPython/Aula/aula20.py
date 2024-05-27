3f0a141de0aa422d195e208cd9546fd2c47c97a3
Italo_Leonardo <italoleonardo69@gmail.com>
2024-05-27 09:09:49 -0300

    add

---
+++ CursoPython/Aula/aula20.py
@@ -0,0 +1,20 @@
+'''def lin():
+    print("-"*30)
+
+lin()
+print("CURSO EM VIDEO")
+lin()'''
+
+'''def titulo(txt):
+    print("-"*30)
+    print(txt)
+    print("-"*30)
+
+titulo("Ola munda")'''
+
+def soma(a, b):
+    print(f"A = {a} e B = {b}")
+    s = a + b
+    print(f"A soma A + B = {s}")
+
+soma(5, 5)
