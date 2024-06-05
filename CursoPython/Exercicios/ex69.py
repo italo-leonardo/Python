e3ef0d23af0b1dc17f4ea162b010be09b6a4afea
Italo_Leonardo <italoleonardo69@gmail.com>
2024-06-05 10:16:09 -0300

    add

--- CursoPython/Exercicios/ex69.py
+++ CursoPython/Exercicios/ex69.py
@@ -5,14 +5,19 @@ while True:
     print('CADASTRE UMA PESSOA')
     print('-'*30)
     idade = int(input('Idade: '))
-    sexo = str(input('Sexo: [F/M] ')).upper()
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [F/M] ')).upper().strip()[0]
     if idade >= 18:
         cont += 1
     if sexo == "M":
         cont_1 += 1
     if sexo == "F" and idade < 18:
         cont_2 += 1
-    perguta = str(input('Quer continuar? [S/N]')).upper()
+        
+    perguta = ' '   
+    while perguta not in 'SN':
+        perguta = str(input('Quer continuar? [S/N]')).upper().strip()[0]
     if perguta == "N":
         break 
 print(f'Total de pessoas com mais de 18 anos: {cont}')
