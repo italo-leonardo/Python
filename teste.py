0d28f986ca576622f01d2471f84a0e3b6b8b7c21
Italo_Leonardo <italoleonardo69@gmail.com>
2024-06-06 10:56:39 -0300

    add

--- teste.py
+++ teste.py
@@ -1,25 +1,10 @@
-n = int(input())
-total = n
-ced = 100
-totcad = 0
-while True:
-    if total >= ced:
-        total -= ced
-        totcad += 1
-    else:
-        print(f'{totcad} nota (s) de R${ced:.2f}')
-        if ced == 100:
-            ced = 50
-        elif ced == 50:
-            ced = 20
-        elif ced == 20:
-            ced = 10
-        elif ced == 10:
-            ced = 5
-        elif ced == 5:
-            ced == 2
-        elif ced == 2:
-            ced == 1
-        totcad = 0
-        if total == 0:
-            break
+print("jj")
+cont100 = 0
+valor = int(input())
+total = valor
+
+while total > 0:
+    if total >= 100:
+        total -= 100
+        cont100 +=1 
+print(cont100, valor, total)
