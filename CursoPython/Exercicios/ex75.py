bc3265fbc856dd47ea05358ed5dceb378c5f4b5d
Italo_Leonardo <italoleonardo69@gmail.com>
2024-06-18 19:47:05 -0300

    add

--- CursoPython/Exercicios/ex75.py
+++ CursoPython/Exercicios/ex75.py
@@ -1,14 +1,13 @@
 lista = (int(input('Digite um número: ')), int(input('Digite outro número: ')), int(input('Digite mais um número: ')), int(input('Digite o último número: ')))
-lista2 = 0
-cont9 = cont3 = 0
-
-for valor in lista:
-    if valor == 9:
-        cont9 += 1
-    if valor % 2 == 0:
-        lista2 += valor
 
 print(f'Você digitou os valores: {lista}')
-print(f'O valor 9 apareceu {cont9} vezes')
-print(f'O valor 3 {cont3}')
-print(f'Os valores pares digitados foram {lista2}')
+print(f'O valor 9 apareceu {lista.count(9)} vezes')
+if 3 in lista:
+    print(f'O valor 3 apareceu na {lista.index(3)+1}ª posição')
+else:
+    print('O Valor 3 não foi digitado em nenhuma posição')
+print('Os valores pares digitaros foram ', end='')
+for n in lista:
+    if n % 2 == 0:
+        print(n , end=' ')
+print('')
---
+++ CursoPython/Exercicios/ex76.py
@@ -0,0 +1,9 @@
+lista = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90, 'Estojo', 25.00, 'Transferidor', 4.20, 'Compasso', 9.99, 'Mocila', 120.32, 'Canetas', 22.30, 'Livros', 34.30)
+print('-' * 40)
+print(f'{"LISTAGUEM DE PREÇO":^40}')
+print('-' * 40)
+for pos in range(0, len(lista)):
+    if pos % 2 == 0:
+        print(f'{lista[pos]:.<30}', end='')
+    else:
+        print(f'R${lista[pos]:>7}')
