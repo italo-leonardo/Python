8680cbb549a96668a1ab535aa8378f29d0bd90f6
Italo_Leonardo <italoleonardo69@gmail.com>
2024-06-18 10:43:58 -0300

    add

---
+++ CursoPython/Exercicios/ex72.py
@@ -0,0 +1,10 @@
+num = ('um', 'dois', 'tres', 'quatro')
+
+while True:
+    op = int(input('Digite um numero entre 0 e 4: '))
+    if op < 0 or op > 4:
+       print('Tente novamente.', end='')
+    else:
+        print(f'Vocé digitou o número {num[op]}')
+        break
+
