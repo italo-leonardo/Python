60173730d1cd0bf7915674aa11de2b43ed33fab3
Italo_Leonardo <italoleonardo69@gmail.com>
2024-06-20 00:05:46 -0300

    add

---
+++ teste.py
@@ -0,0 +1,10 @@
+h, hf = map(int, input().split())
+d = 0
+if h >= hf:
+    for i in range(h, 24 + hf):
+        d += 1
+else:
+    for i in range(h, hf - h + 2):
+        d += 1
+print(f'O JOGO DUROU {d} HORA(S)')
+            
