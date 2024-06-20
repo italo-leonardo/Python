f79f950b773504eefa2833d61c04bf64ba6f9bf6
Italo_Leonardo <italoleonardo69@gmail.com>
2024-06-19 21:38:36 -0300

    add

---
+++ CursoPython/Exercicios/ex79.py
@@ -0,0 +1,22 @@
+valores = []
+while True:
+    num = int(input('Digite um valor: '))
+    if num not in valores:
+        valores.append(num)
+        print('Valor adicionado com sucesso...')
+        op = input('Que continuar? [S/N]').upper()
+        if op == 'S':
+            continue
+        else:
+            break 
+    else:
+        print('Valor duplicado! Não vou adiconar...')
+        op = input('Que continuar? [S/N]').upper()
+        if op == 'S':
+            continue
+        else:
+            break 
+        
+print('-='*30)
+print(f'Você digitou os valores {sorted(valores)}')
+
