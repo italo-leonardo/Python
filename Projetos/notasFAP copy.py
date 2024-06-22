279053e3466d44aa1fb9aef0e6b53f31432136d6
Italo_Leonardo <italoleonardo69@gmail.com>
2024-06-22 11:40:43 -0300

    add

--- Projetos/notasFAP copy.py
+++ Projetos/notasFAP copy.py
@@ -1,8 +1,6 @@
 def ponto():
     print('-='*15)
-def prime_decimal():
-    prime_decimal = int(media * 10) % 10
-    return prime_decimal
+
 
 disciplina = str(input('Digite o nome da Disciplina: '))
 av1 = float(input('Digite seu nota da AV1: '))
@@ -12,15 +10,25 @@ tde2 = float(input('Digite seu nota do TDE2: '))
 tde3 = float(input('Digite seu nota do TDE3: '))
 tde4 = float(input('Digite seu nota do TDE4: '))
 
-media = ((av1 * 0.4) + (av2 * 0.4)) + (0.05 * (tde1 + tde2 + tde3 + tde4) ) 
+media = ((av1 * 0.4) + (av2 * 0.4)) + (0.05 * (tde1 + tde2 + tde3 + tde4) )   
+prime_decimal = int(media * 10) % 10
 
-if media >= 7.2 and media <= 7.5:
-    while media == 7.5:
-        media += 0.1
-         
+
+if prime_decimal >= 2 and prime_decimal <= 5:
+    while prime_decimal != 5:
+        prime_decimal += 1
+    print(prime_decimal)
+elif prime_decimal >= 7:
+    while prime_decimal != 10:
+        prime_decimal += 1
+    prime_decimal /= 10
+    print(prime_decimal)
+else: 
+    prime_decimal -= 1
+    print(prime_decimal)
+
     
-print(media)
-print(prime_decimal())
+
 ponto()
 print(f'{"SIMULADOR DE NOTAS":^30}')
 ponto()
