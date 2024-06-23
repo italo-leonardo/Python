2db5ed25d4b375241c456bf3cb7e90cae0465c2c
Italo_Leonardo <italoleonardo69@gmail.com>
2024-06-23 09:45:11 -0300

    add

--- CursoPython/Exercicios/ex84.py
+++ CursoPython/Exercicios/ex84.py
@@ -13,4 +13,4 @@ while True:
 
 print('-='*30)
 print(f'Ao todo, você cadastrou {len(pessoas)} pessoas.')
-print(f'O manior peso foi de {max(pessoas)}Kg.')
+print(f'O manior peso foi de {max(pessoas[0])}Kg.')
