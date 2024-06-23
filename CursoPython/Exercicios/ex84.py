dba2ef4a6bc124c2c2f7ff5d1e3d84a813df282e
Italo_Leonardo <italoleonardo69@gmail.com>
2024-06-23 09:21:58 -0300

    add

---
+++ CursoPython/Exercicios/ex84.py
@@ -0,0 +1,15 @@
+pessoas = list()
+dados = list()
+while True:
+    nome = str(input('Nome: '))
+    peso = float(input('Peso: '))
+    dados.append(nome)
+    dados.append(peso)
+    pessoas.append(dados[:])
+    dados.clear()
+    op = str(input('Quer continuar? [S/N]: ')).upper()
+    if op != 'S':
+        break
+
+print('-='*30)
+print(f'Ao todo, você cadastrou {len(pessoas)} pessoas.')
