8f4459c39c0845342373ff1d9f32ab15953bd5be
Italo_Leonardo <italoleonardo69@gmail.com>
2024-07-07 10:56:10 -0300

    add

---
+++ CursoPython/Exercicios/ex98.py
@@ -0,0 +1,33 @@
+from time import sleep
+
+def contagem(i, f, p):
+    print('-='*30)
+    print(f'Contador de {i} até {f} de {p} em {p}')
+    if p < 0:
+        p *= -1
+    if p == 0:
+        p = 1
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.3)
+            cont += p
+        print('FIM!')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.3)
+            cont -= p
+        print('FIM!')
+    print('-='*30)
+
+
+contagem(1, 10, 1)
+contagem(10,0,2)
+print('Agora é sua vez de personalizar a contagem!')
+inic = int(input('Início: '))
+fim = int(input('Fim: '))
+pas = int(input('Passo: '))
+contagem(inic, fim, pas)
