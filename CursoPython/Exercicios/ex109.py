1ba186a89fe743e2aefea99f3e1b37a8c25f6b9b
Italo_Leonardo <italoleonardo69@gmail.com>
2024-07-22 11:51:57 -0300

    add

--- CursoPython/Exercicios/ex109.py
+++ CursoPython/Exercicios/ex109.py
@@ -1,8 +1,7 @@
 import uteis1
 
 num = float(input('Digite o preço: R$'))
-
-print(f'A metade de {(num)} é {uteis1.metade(num, True)}')
+print(f'A metade de {num} é {uteis1.metade(num, True)}')
 print(f'O dobro de {num} é {uteis1.dobro(num, True)}')
 print(f'Aumentando 10%, temos {uteis1.aumento(num, 10, True)}')
 print(f'Diminuição 10%, temos {uteis1.dimunuir(num, 10, True)}')
--- CursoPython/Exercicios/uteis1.py
+++ CursoPython/Exercicios/uteis1.py
@@ -1,19 +1,19 @@
 def metade(n, formato=False):
     m = n / 2
-    return m if formato is False else moeda(n)
+    return m if formato is False else moeda(m)
 
 def dobro(n, formato=False):
     d = n * 2
-    return d if formato is False else moeda(n)
+    return d if formato is False else moeda(d)
 
 def aumento(n, taxa, formato=False):
     v = n * (taxa/100)
     s  = v + n
-    return s if formato is False else moeda(n)
+    return s if formato is False else moeda(s)
 
 def dimunuir(n, taxa, formato=False):
     di = n - (n * (taxa/100))
-    return di if formato is False else moeda(n)
+    return di if formato is False else moeda(di)
 
 def moeda(n = 0, moeda = 'R$'):
     return f'{moeda}{n:.2f}'.replace('.', ',')
