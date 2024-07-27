4e2fa606d575f46e75247a2a0b23b3947011ff6e
Italo_Leonardo <italoleonardo69@gmail.com>
2024-07-27 18:27:17 -0300

    add

---
+++ CursoPython/DankiCode/associacao.py
@@ -0,0 +1,16 @@
+"""
+Operadores de Associacao
+
+nome = 'marcos'
+
+Operador in - x in y
+Operador not in - x not in y
+
+"""
+
+x = 'pernambuco'
+
+y = "per"
+
+print(y in x)
+print('z' not in x)
---
+++ CursoPython/DankiCode/input.py
@@ -0,0 +1,4 @@
+#input recebe dados do usuario
+nome = input('Digite seu nome: ')
+idade = int(input('Digite seu idade: '))
+print(f'Ola, {nome} com {idade} anos!')
