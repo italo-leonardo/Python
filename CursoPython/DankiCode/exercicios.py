24031acaeead25929320abe9aef4a9a4c281232a
Italo_Leonardo <italoleonardo69@gmail.com>
2024-07-27 18:53:16 -0300

    add

---
+++ CursoPython/DankiCode/exercicios.py
@@ -0,0 +1,35 @@
+#01 - area de um retangulo
+"""altura = int(input('Digite a altura: '))
+base = int(input('Digite a base: '))
+area = altura * base
+print(f'A area desse retangulo e {area}')"""
+
+#02 - are de um quadrado
+"""lado = int(input('Digite o labo: '))
+area = lado ** 2
+print(f'A area do quadrado e {area}')"""
+
+#03 - desconto no produto
+'''produto = float(input('Digite o valor do produto: '))
+desconto = float(input('Digete o desconto em %: '))
+valor = produto * (desconto / 100)
+preco_final = produto - valor
+print(f'Com um desconto de {desconto}% o valor fica {preco_final}')'''
+
+#04 - area de um circulo
+'''raio = int(input('Qual e o valor do raio: '))
+pi = 3.14
+area = pi * (raio ** 2)
+print(f'A era do circulo e {area}')'''
+
+#05 - reais para dolar
+'''reais = float(input('Quantos reais que converter: '))
+dolar  = 5.64 
+converte = reais / dolar
+print(f'R${reais} da U${converte}')'''
+
+#06 - dolar para reais
+dolar = float(input('Quantos reais que converter: '))
+reais  = 5.64 
+converte = reais * dolar
+print(f'U${dolar} da R${converte}')
