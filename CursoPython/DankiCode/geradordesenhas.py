18b900a675fbd26fd193131b832e911e5cd6a052
Italo_Leonardo <italoleonardo69@gmail.com>
2024-07-28 11:26:25 -0300

    add

--- CursoPython/DankiCode/geradordesenhas.py
+++ CursoPython/DankiCode/geradordesenhas.py
@@ -12,13 +12,14 @@ chave = input('Digite a base da sua senha: ')
 senha = ''
 
 for letra in chave:
-    if letra in 'Aa': senha = senha + '10'
-    elif letra in 'Bb': senha = senha + '11'
-    elif letra in 'Cc': senha = senha + '12'
-    elif letra in 'Dd': senha = senha + '13'
-    elif letra in 'Ee': senha = senha + '14'
+    if letra in 'Aa': senha = senha + '0'
+    elif letra in 'Bb': senha = senha + '1'
+    elif letra in 'Cc': senha = senha + '2'
+    elif letra in 'Dd': senha = senha + '8'
+    elif letra in 'Ee': senha = senha + '4'
     elif letra in 'Ff': senha = senha + '#'
     elif letra in 'Rr': senha = senha + '$'
+    elif letra in ' ': senha = senha + '_'
     else: senha = senha + letra
 print(senha)
     
