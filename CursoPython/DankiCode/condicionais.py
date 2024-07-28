4de7fb92c2ef1b6ac69e2e6906cf666ded635c21
Italo_Leonardo <italoleonardo69@gmail.com>
2024-07-28 09:44:51 -0300

    add

---
+++ CursoPython/DankiCode/condicionais.py
@@ -0,0 +1,25 @@
+"""
+Python - Comandos de controle condicionnal
+
+if, else, elif 
+"""
+'''
+x = 5
+y = 5
+
+if x > y:
+    print(f'O x e maior que o y')
+elif x < y:
+    print('x e menor que y')
+else:
+    print('x e igual a y')
+print('Codigo fora do bloco condicional')
+'''
+
+a = 5
+b = 8
+
+'''if b > a: print('b e maior que a')
+print('Codigo fora do bloco if')'''
+
+print('B') if b > a else print('A') # Operador ternario, Expressoes condiciona;.
---
+++ CursoPython/DankiCode/exercicio2.py
@@ -0,0 +1,23 @@
+# 01 - Criar a variável 
+'''idade = int(input('Digite sua idade: '))
+
+if idade < 16:
+    print('Menor de idade')
+elif idade >= 16 and idade < 18:
+    print('Emancipado')
+else:
+    print('Maior de idade')'''
+
+# 02 - Buscar a idade de nadador e classificar na tabela: 
+idade = int(input('Digite seu idade: '))
+
+if idade > 5 and idade < 7:
+    print('Infantil A')
+elif idade > 8 and idade < 10:
+    print('Infantil B')
+elif idade > 11  and idade < 13:
+    print('Juvenil A')
+elif idade > 14 and idade < 17:
+    print('Juvenil B')
+
+
