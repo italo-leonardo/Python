cc9ec3104700f9bcb7d877f7d68bc126f791df02
Italo_Leonardo <italoleonardo69@gmail.com>
2024-07-28 10:34:04 -0300

    add

---
+++ CursoPython/DankiCode/for.py
@@ -0,0 +1,21 @@
+"""
+Estruturas de repeticao - loops
+
+while 
+for 
+do while*
+"""
+'''
+s = 'pernambuco'
+for x in s:
+    print(x)
+'''
+
+'''for x in range(6):
+    print(x)'''
+
+'''for x in range(5, 8):
+    print(x)'''
+
+for x in range(2,10,5):
+    print(x)
---
+++ CursoPython/DankiCode/while.py
@@ -0,0 +1,14 @@
+"""
+Estruturas de repeticao - loops
+
+while 
+for 
+do while*
+"""
+a = 0
+
+while a <= 10:
+    print(a)
+    if a == 3:
+        break
+    a += 1
