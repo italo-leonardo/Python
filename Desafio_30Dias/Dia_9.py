ba6c236b6c7921b0e5ce82c0874de1995138df04
Italo_Leonardo <italoleonardo69@gmail.com>
2024-08-22 20:40:56 -0300

    add

---
+++ Desafio_30Dias/Dia_9.py
@@ -0,0 +1,13 @@
+positivo = 0
+total = 0
+
+for num in range(1, 7):
+    num = float(input())
+    if num >= 0:
+        positivo += 1
+        total += num
+        
+media = total / positivo
+
+print(f'{positivo} valores positivos')
+print(f'{media:.2f}')
