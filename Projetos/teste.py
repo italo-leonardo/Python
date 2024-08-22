d3b49a21a1a0eb1d3eb12b8bd912aeed7c662f74
Italo_Leonardo <italoleonardo69@gmail.com>
2024-08-22 20:35:43 -0300

    add

--- Projetos/teste.py
+++ Projetos/teste.py
@@ -1,6 +1,13 @@
-cont = 0
-for i in range(1, 7):
-    n = int(input())
-    if n > 0:
-        cont += 1
-print(f"{cont} valores positivos")
+positivo = 0
+total = 0
+
+for num in range(1, 7):
+    num = float(input())
+    if num >= 0:
+        positivo += 1
+        total += num
+        
+media = total / positivo
+
+print(f'{positivo} valores positivos')
+print(f'{media}:.1f')
