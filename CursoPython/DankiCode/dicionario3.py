46cedc7f5fb6e4b5458aa70ea0dff48b082527c1
Italo_Leonardo <italoleonardo69@gmail.com>
2024-08-25 12:13:55 -0300

    add

---
+++ CursoPython/DankiCode/dicionario3.py
@@ -0,0 +1,17 @@
+'''tupla = ('item1', 'item2', 'item3')
+tupla2 = ('item1', 'item2', 'item3')
+dicio = dict.fromkeys(tupla, tupla2)
+print(dicio)'''
+
+dicio = {
+    'dicio1': {
+        'nome': 'Pedro',
+        'idade': 23
+    },
+    'dicio2': {
+        'nome': 'Jhuly',
+        'idade': 20
+    }
+}
+
+print(dicio)
