87f104cc566d1f8ee3d0f13774b01bfbb982d26b
Italo_Leonardo <italoleonardo69@gmail.com>
2024-12-14 19:43:21 -0300

    add

---
+++ CursoPython/CursoEmVideo/Aula/aula12.py
@@ -0,0 +1,12 @@
+"""
+Estrutura de desicao Python 3.10
+"""
+media = int(input())
+
+match media:
+    case media if media < 5:
+        print("Voce foi reprovado")
+    case media if 5 <= media < 7:
+        print("Voce fara a recuperacao")
+    case media if media >= 7:
+        print("Voce foi aprovado")
