3b13b65170bbcfb78d65cfa870ac6aca597ae18a
Italo_Leonardo <italoleonardo69@gmail.com>
2024-12-22 19:43:37 -0300

    add

--- Projetos/teste.py
+++ Projetos/teste.py
@@ -1,9 +1,4 @@
-media = int(input())
-
-match media:
-    case media if media < 5:
-        print("Voce foi reprovado")
-    case media if 5 <= media < 7:
-        print("Voce fara a recuperacao")
-    case media if media >= 7:
-        print("Voce foi aprovado")
+for i in range(1,6):
+    if i == 4:
+        continue
+    print(i)
